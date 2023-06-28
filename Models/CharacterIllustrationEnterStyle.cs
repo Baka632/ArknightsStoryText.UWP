48cf1851d3318770db5bf9880d0b1eb1f150eeb0
Baka632 <stevemc123456@outlook.com>
2023-06-28 09:40:51 +0800

    移动 StoryReader 的位置，为某些来自 ArkRes.Utility 项目的文件添加了说明

--- Models/CharacterIllustrationEnterStyle.cs
+++ Models/CharacterIllustrationEnterStyle.cs
@@ -1,4 +1,7 @@
-﻿namespace ArknightsResources.Stories.Models
+﻿//原项目：https://github.com/ArknightsResources/Utility
+//为了兼容 Windows 10 1703及以下版本， 我们无法直接引用 NuGet 包，所以只能复制源代码了（
+
+namespace ArknightsResources.Stories.Models
 {
     /// <summary>
     /// 表示干员立绘进入方式的枚举
--- Models/StoryScene.cs
+++ Models/StoryScene.cs
@@ -1,4 +1,5 @@
 ﻿//原项目：https://github.com/ArknightsResources/Utility
+//为了兼容 Windows 10 1703及以下版本， 我们无法直接引用 NuGet 包，所以只能复制源代码了（
 
 using ArknightsResources.Stories.Models.Commands;
 using System.Collections;
