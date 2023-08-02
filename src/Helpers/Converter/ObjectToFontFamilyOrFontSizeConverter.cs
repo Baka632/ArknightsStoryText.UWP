2a7e7db0151fae124101492e2a602c2e9aa73fe4
Baka632 <stevemc123456@outlook.com>
2023-08-02 14:42:16 +0800

    添加打开文件夹功能

---
+++ src/Helpers/Converter/ObjectToFontFamilyOrFontSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ArknightsStoryText.UWP.Helpers.Converter
+{
+    public class ObjectToFontFamilyOrDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value switch
+            {
+                FontFamily fontFamily => fontFamily,
+                double fontSize => fontSize,
+                _ => DependencyProperty.UnsetValue
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
